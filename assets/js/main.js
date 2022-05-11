712f5876f010d8e721c5f443cc967995224e47e3
StephanPagel <stephan.pagel84@gmail.com>
2022-05-11 08:43:27 +0200

    finished

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+let headline = document.getElementById('element');
+
+function small() {
+    headline.className = '';
+    headline.classList.add('small');
+}
+
+function middle() {
+    headline.className = '';
+    headline.classList.add('middle');
+}
+function big() {
+    headline.className = '';
+    headline.classList.add('big');
+}
